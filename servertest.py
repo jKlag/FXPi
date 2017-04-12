bea11aa92c3d9482c4d6d082766bb8dea8eb3305
Josh Klag <joshua.klag@vanderbilt.edu>
2017-04-12 17:37:41 -0500

    basic functionality

--- servertest.py
+++ servertest.py
@@ -11,20 +11,29 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
 
     def handle(self):
+        EFFECTS = {"DSP":0,"WAH":1,"VIBRATO":2}
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print "{} wrote:".format(self.client_address[0])
         print self.data
+        dataarr = self.data.split(' ')
+        effect = EFFECTS[dataarr[0].upper()]
+        cmd = 0
+        if dataarr[1].upper() == 'ON':
+            cmd = 1
+
+
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
-        os.system("echo '0 " + self.data + ";' | /Applications/Pd-0.47-1-64bit.app/Contents/Resources/bin/pdsend 3000")
+        os.system("echo '" + str(effect) + ' ' + str(cmd) + ";' | /Applications/Pd-0.47-1-64bit.app/Contents/Resources/bin/pdsend 3000")
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "localhost", 9996
 
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
+    os.system("echo '1 0;' | /Applications/Pd-0.47-1-64bit.app/Contents/Resources/bin/pdsend 3000")
+    os.system("echo '2 0;' | /Applications/Pd-0.47-1-64bit.app/Contents/Resources/bin/pdsend 3000")
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
